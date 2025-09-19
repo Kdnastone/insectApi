16d261922c2dc03a0ae820f53b0904002abe3952
sandra <sandra.cadena97@soy.sena.edu.co>
2025-09-19 07:00:08 -0500

    feat: install cors

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 // Middleware global para permitir solo GET
